a478f69082156a6c59f671491486107c1e716f6c
andrew <andrewjohnlay@gmail.com>
2023-03-12 16:29:23 +1100

    Debugging

--- src/debugHelper.c
+++ src/debugHelper.c
@@ -5,7 +5,7 @@ extern boolean* flag;
 extern byte* var;
 
 long opCounter = 1;
-long stopAt = 2000;
+long stopAt = 1000;
 long exitAt = 2500;
 boolean stopEvery = FALSE;
 
@@ -20,7 +20,7 @@ void stopAtFunc()
 #pragma code-name (push, "BANKRAM05");
 void debugPrint(byte toPrint)
 {
-	printf("Op %lu, %d, VAR 0 is %d\n", opCounter, toPrint, var);
+	printf("op %lu, %d, var 0 is %d\n", opCounter, toPrint, var[0]);
 
 	if (stopEvery)
 	{
@@ -44,22 +44,22 @@ void debugPrint(byte toPrint)
 
 void debugPrintFalse()
 {
-	printf("The result is false\n");
+	printf("the result is false\n");
 }
 
 void debugPrintTrue()
 {
-	printf("The result is true\n");
+	printf("the result is true\n");
 }
 
 void debugPrintNot()
 {
-	printf("The result is inverted by not\n");
+	printf("the result is inverted by not\n");
 }
 
 void debugPrintOrMode()
 {
-	printf("Or Mode Started\n");
+	printf("or mode started\n");
 }
 
 
@@ -68,12 +68,12 @@ extern byte logDebugVal2;
 
 void debugIsSet()
 {
-	printf("Checking that %d is set and it %d\n", logDebugVal1, flag[logDebugVal1]);
+	printf("checking that %d is set and it %d\n", logDebugVal1, flag[logDebugVal1]);
 }
 
 void debugGreaterThan_8N()
 {
-	printf("Checking that %d (%d) is > %d and the result should be %d\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal1] > logDebugVal2);
+	printf("checking that %d (%d) is > %d and the result should be %d\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal1] > logDebugVal2);
 }
 
 void debugLessThan_8N()
@@ -83,83 +83,83 @@ void debugLessThan_8N()
 
 void debugGreaterThan_8V()
 {
-	printf("Checking that %d (%d) is > %d and the result should be %d\n", logDebugVal1, var[logDebugVal1] , logDebugVal2, var[logDebugVal2], var[logDebugVal1] > var[logDebugVal2]);
+	printf("checking that %d (%d) is > %d and the result should be %d\n", logDebugVal1, var[logDebugVal1] , logDebugVal2, var[logDebugVal2], var[logDebugVal1] > var[logDebugVal2]);
 }
 
 void debugLessThan_8V()
 {
-	printf("Checking that %d is < %d and the result should be %d\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2], var[logDebugVal1] < var[logDebugVal2]);
+	printf("checking that %d is < %d and the result should be %d\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2], var[logDebugVal1] < var[logDebugVal2]);
 }
 
 void debugEqualN()
 {
-	printf("Checking that %d is equal to var %d (%d) and it % d\n", logDebugVal1, var[logDebugVal2], logDebugVal1 == var[logDebugVal2]);
+	printf("checking that %d is equal to var %d (%d) and it % d\n", logDebugVal1, var[logDebugVal2], logDebugVal1 == var[logDebugVal2]);
 }
 
 void debugEqualV()
 {
-	printf("Checking that %d (%d) is equal to var %d (%d) and it % d\n", var[logDebugVal1], var[logDebugVal2], var[logDebugVal1] == var[logDebugVal2]);
+	printf("checking that %d (%d) is equal to var %d (%d) and it % d\n", var[logDebugVal1], var[logDebugVal2], var[logDebugVal1] == var[logDebugVal2]);
 }
 
 void debugInc()
 {
-	printf("Incrementing var %d(%d) to %d\n", logDebugVal1, var[logDebugVal1], var[logDebugVal1] + 1);
+	printf("incrementing var %d(%d) to %d\n", logDebugVal1, var[logDebugVal1], var[logDebugVal1] + 1);
 }
 
 void debugDec()
 {
-	printf("Decrementing var %d(%d) to %d\n", logDebugVal1, var[logDebugVal1], var[logDebugVal1] - 1);
+	printf("decrementing var %d(%d) to %d\n", logDebugVal1, var[logDebugVal1], var[logDebugVal1] - 1);
 }
 
 void debugAddN()
 {
-	printf("Add var %d (%d) to %d which is", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal1] + logDebugVal2);
+	printf("add var %d (%d) to %d which is", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal1] + logDebugVal2);
 }
 
 void debugAddV()
 {
-	printf("Add var %d (%d) to %d (%d) which is\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2], var[logDebugVal1] + var[logDebugVal2]);
+	printf("add var %d (%d) to %d (%d) which is\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2], var[logDebugVal1] + var[logDebugVal2]);
 }
 
 void debugSubN()
 {
-	printf("Sub var %d (%d) to %d which is\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal1] - logDebugVal2);
+	printf("sub var %d (%d) to %d which is\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal1] - logDebugVal2);
 }
 
 void debugSubV()
 {
-	printf("Sub var %d (%d) to %d (%d) which is\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2], var[logDebugVal1] - var[logDebugVal2]);
+	printf("sub var %d (%d) to %d (%d) which is\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2], var[logDebugVal1] - var[logDebugVal2]);
 }
 
 void debugAssignN()
 {
-	printf("Assign var %d (%d) to %d\n", logDebugVal1, var[logDebugVal1], logDebugVal2);
+	printf("assign var %d (%d) to %d\n", logDebugVal1, var[logDebugVal1], logDebugVal2);
 }
 
 void debugAssignV()
 {
-	printf("Assign var %d (%d) to %d (%d) which is\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2]);
+	printf("assign var %d (%d) to %d (%d) which is\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2]);
 }
 
 void debugIndirect()
 {
-	printf("Indir %d (%d) value %d\n", logDebugVal1, var[logDebugVal1], logDebugVal2);
+	printf("indir %d (%d) value %d\n", logDebugVal1, var[logDebugVal1], logDebugVal2);
 }
 
 void debugIndirectV()
 {
-	printf("Indir V %d (%d) value %d (%d)\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2]);
+	printf("indir V %d (%d) value %d (%d)\n", logDebugVal1, var[logDebugVal1], logDebugVal2, var[logDebugVal2]);
 }
 
 
 void debugPostCheckVar()
 {
-	printf("Post check var %d (%d)\n", logDebugVal1, var[logDebugVal1]);
+	printf("post check var %d (%d)\n", logDebugVal1, var[logDebugVal1]);
 }
 
 void debugPostCheckFlag()
 {
-	printf("Post check flag %d (%d)\n", logDebugVal1, flag[logDebugVal1]);
+	printf("post check flag %d (%d)\n", logDebugVal1, flag[logDebugVal1]);
 
 }
 
@@ -170,7 +170,7 @@ void codeJumpDebug()
 #pragma code-name (pop);
 void debugPrintCurrentCodeState(byte* code)
 {
-	printf("The code is now %u and the address is %p\n", *code, code);
+	printf("the code is now %u and the address is %p\n", *code, code);
 }
 
 
