17bc9ebad9731cc560f206030cd6b11b88ba4209
andrew <andrewjohnlay@gmail.com>
2023-07-10 20:48:10 +1000

    Fixed issue causing screen to be too low

--- src/picture.s
+++ src/picture.s
@@ -116,10 +116,10 @@ sta _toDraw     ; toDraw = picColour << 4 | picColour
 
 lda coX
 clc
-adc #<STARTING_BYTE
+adc #<(STARTING_BYTE / 2)
 sta _drawWhere
 lda #$0
-adc #>STARTING_BYTE
+adc #>(STARTING_BYTE / 2)
 sta _drawWhere+1
 
 clc
