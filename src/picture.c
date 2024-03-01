0eabed2ee5783fd7a808ba836ec112b57a59a3df
andrew <andrewjohnlay@gmail.com>
2024-03-01 22:09:34 +1100

    Now correctly drawing the stars

--- src/picture.c
+++ src/picture.c
@@ -46,6 +46,25 @@ extern long pixelCounter;
 extern long pixelStartPrintingAt;
 #endif // VERBOSE_FLOOD
 
+
+/**************************************************************************
+** pset
+**
+** Draws a pixel in each screen depending on whether drawing in that
+** screen is enabled or not.
+**************************************************************************/
+#define PSET(x, y) \
+    if (picDrawEnabled) { \
+if ((x) <= 159 && (y) <= 167) {  \
+             toDraw = picColour << 4 | picColour; \
+             drawWhere = (STARTING_BYTE + x) + (bitmapWidthPreMult[y]);  \
+			SET_VERA_ADDRESS_ABSOLUTE(drawWhere, ADDRESSSEL0, 0) \
+			 WRITE_BYTE_VAR_TO_ASSM(toDraw, VERA_data0); \
+            \
+           } \
+    }
+
+
 void getLoadedPicture(PictureFile* returnedloadedPicture, byte loadedPictureNumber)
 {
 	byte previousRamBank = RAM_BANK;
@@ -204,6 +223,96 @@ void testQueue()
 #pragma code-name (pop)
 
 #pragma code-name (push, "BANKRAM04")
+
+
+#define plotPatternPoint() \
+   if (patCode & 0x20) { \
+      if ((splatterMap[bitPos>>3] >> (7-(bitPos&7))) & 1) PSET(x1, y1); \
+      bitPos++; \
+      if (bitPos == 0xff) bitPos=0; \
+   } else PSET(x1, y1)
+
+#pragma wrapped-call (push, trampoline, PICTURE_CODE_OVERFLOW_BANK)
+/**************************************************************************
+** plotPattern
+**
+** Draws pixels, circles, squares, or splatter brush patterns depending
+** on the pattern code.
+**************************************************************************/
+void b4PlotPattern(byte x, byte y)
+{
+	static char circles[][15] = { /* agi circle bitmaps */
+	  {0x80},
+	  {0xfc},
+	  {0x5f, 0xf4},
+	  {0x66, 0xff, 0xf6, 0x60},
+	  {0x23, 0xbf, 0xff, 0xff, 0xee, 0x20},
+	  {0x31, 0xe7, 0x9e, 0xff, 0xff, 0xde, 0x79, 0xe3, 0x00},
+	  {0x38, 0xf9, 0xf3, 0xef, 0xff, 0xff, 0xff, 0xfe, 0xf9, 0xf3, 0xe3, 0x80},
+	  {0x18, 0x3c, 0x7e, 0x7e, 0x7e, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7e, 0x7e,
+	   0x7e, 0x3c, 0x18}
+	};
+
+	static byte splatterMap[32] = { /* splatter brush bitmaps */
+	  0x20, 0x94, 0x02, 0x24, 0x90, 0x82, 0xa4, 0xa2,
+	  0x82, 0x09, 0x0a, 0x22, 0x12, 0x10, 0x42, 0x14,
+	  0x91, 0x4a, 0x91, 0x11, 0x08, 0x12, 0x25, 0x10,
+	  0x22, 0xa8, 0x14, 0x24, 0x00, 0x50, 0x24, 0x04
+	};
+
+	static byte splatterStart[128] = { /* starting bit position */
+	  0x00, 0x18, 0x30, 0xc4, 0xdc, 0x65, 0xeb, 0x48,
+	  0x60, 0xbd, 0x89, 0x05, 0x0a, 0xf4, 0x7d, 0x7d,
+	  0x85, 0xb0, 0x8e, 0x95, 0x1f, 0x22, 0x0d, 0xdf,
+	  0x2a, 0x78, 0xd5, 0x73, 0x1c, 0xb4, 0x40, 0xa1,
+	  0xb9, 0x3c, 0xca, 0x58, 0x92, 0x34, 0xcc, 0xce,
+	  0xd7, 0x42, 0x90, 0x0f, 0x8b, 0x7f, 0x32, 0xed,
+	  0x5c, 0x9d, 0xc8, 0x99, 0xad, 0x4e, 0x56, 0xa6,
+	  0xf7, 0x68, 0xb7, 0x25, 0x82, 0x37, 0x3a, 0x51,
+	  0x69, 0x26, 0x38, 0x52, 0x9e, 0x9a, 0x4f, 0xa7,
+	  0x43, 0x10, 0x80, 0xee, 0x3d, 0x59, 0x35, 0xcf,
+	  0x79, 0x74, 0xb5, 0xa2, 0xb1, 0x96, 0x23, 0xe0,
+	  0xbe, 0x05, 0xf5, 0x6e, 0x19, 0xc5, 0x66, 0x49,
+	  0xf0, 0xd1, 0x54, 0xa9, 0x70, 0x4b, 0xa4, 0xe2,
+	  0xe6, 0xe5, 0xab, 0xe4, 0xd2, 0xaa, 0x4c, 0xe3,
+	  0x06, 0x6f, 0xc6, 0x4a, 0xa4, 0x75, 0x97, 0xe1
+	};
+
+	int circlePos = 0;
+	byte x1, y1, penSize, bitPos = splatterStart[patNum];
+
+	penSize = (patCode & 7);
+
+	/* Don't know exactly what happens at the borders, but it can definitely
+	** cause problems if it isn't right. */
+	/*
+	if (x<((penSize/2)+1)) x=((penSize/2)+1);
+	else if (x>160-((penSize/2)+1)) x=160-((penSize/2)+1);
+	if (y<penSize) y = penSize;
+	else if (y>=168-penSize) y=167-penSize;
+	*/
+	if (x < penSize) x = penSize - 1;
+	if (y < penSize) y = penSize;
+	//else if (y>=168-penSize) y=167-penSize;
+
+	for (y1 = y - penSize; y1 <= y + penSize; y1++) {
+		for (x1 = x - (penSize + 1) / 2; x1 <= x + penSize / 2; x1++) {
+			if (patCode & 0x10) { /* Square */
+				plotPatternPoint();
+			}
+			else { /* Circle */
+				if ((circles[patCode & 7][circlePos >> 3] >> (7 - (circlePos & 7))) & 1) {
+					plotPatternPoint();
+				}
+				circlePos++;
+			}
+		}
+	}
+
+}
+#pragma wrapped-call (pop)
+
+
 long b4GetVeraPictureAddress(int x, int y)
 {
 	return (STARTING_BYTE + x) + (bitmapWidthPreMult[y]);
@@ -361,23 +470,6 @@ void b11PriPSet(word x, word y)
 	if (y > 167) return;
 	//priority->line[y][x] = priColour;
 }
-
-/**************************************************************************
-** pset
-**
-** Draws a pixel in each screen depending on whether drawing in that
-** screen is enabled or not.
-**************************************************************************/
-#define PSET(x, y) \
-    if (picDrawEnabled) { \
-if ((x) <= 159 && (y) <= 167) {  \
-             toDraw = picColour << 4 | picColour; \
-             drawWhere = (STARTING_BYTE + x) + (bitmapWidthPreMult[y]);  \
-			SET_VERA_ADDRESS_ABSOLUTE(drawWhere, ADDRESSSEL0, 0) \
-			 WRITE_BYTE_VAR_TO_ASSM(toDraw, VERA_data0); \
-            \
-           } \
-    }
 
 unsigned long lineDrawCounter = 0;
 
@@ -822,94 +914,6 @@ byte b11AbsoluteLine(byte** data, BufferStatus* bufferStatus)
 	}
 }
 
-
-#define plotPatternPoint() \
-   if (patCode & 0x20) { \
-      if ((splatterMap[bitPos>>3] >> (7-(bitPos&7))) & 1) PSET(x1, y1); \
-      bitPos++; \
-      if (bitPos == 0xff) bitPos=0; \
-   } else PSET(x1, y1)
-
-/**************************************************************************
-** plotPattern
-**
-** Draws pixels, circles, squares, or splatter brush patterns depending
-** on the pattern code.
-**************************************************************************/
-void b11PlotPattern(byte x, byte y)
-{
-	static char circles[][15] = { /* agi circle bitmaps */
-	  {0x80},
-	  {0xfc},
-	  {0x5f, 0xf4},
-	  {0x66, 0xff, 0xf6, 0x60},
-	  {0x23, 0xbf, 0xff, 0xff, 0xee, 0x20},
-	  {0x31, 0xe7, 0x9e, 0xff, 0xff, 0xde, 0x79, 0xe3, 0x00},
-	  {0x38, 0xf9, 0xf3, 0xef, 0xff, 0xff, 0xff, 0xfe, 0xf9, 0xf3, 0xe3, 0x80},
-	  {0x18, 0x3c, 0x7e, 0x7e, 0x7e, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7e, 0x7e,
-	   0x7e, 0x3c, 0x18}
-	};
-
-	static byte splatterMap[32] = { /* splatter brush bitmaps */
-	  0x20, 0x94, 0x02, 0x24, 0x90, 0x82, 0xa4, 0xa2,
-	  0x82, 0x09, 0x0a, 0x22, 0x12, 0x10, 0x42, 0x14,
-	  0x91, 0x4a, 0x91, 0x11, 0x08, 0x12, 0x25, 0x10,
-	  0x22, 0xa8, 0x14, 0x24, 0x00, 0x50, 0x24, 0x04
-	};
-
-	static byte splatterStart[128] = { /* starting bit position */
-	  0x00, 0x18, 0x30, 0xc4, 0xdc, 0x65, 0xeb, 0x48,
-	  0x60, 0xbd, 0x89, 0x05, 0x0a, 0xf4, 0x7d, 0x7d,
-	  0x85, 0xb0, 0x8e, 0x95, 0x1f, 0x22, 0x0d, 0xdf,
-	  0x2a, 0x78, 0xd5, 0x73, 0x1c, 0xb4, 0x40, 0xa1,
-	  0xb9, 0x3c, 0xca, 0x58, 0x92, 0x34, 0xcc, 0xce,
-	  0xd7, 0x42, 0x90, 0x0f, 0x8b, 0x7f, 0x32, 0xed,
-	  0x5c, 0x9d, 0xc8, 0x99, 0xad, 0x4e, 0x56, 0xa6,
-	  0xf7, 0x68, 0xb7, 0x25, 0x82, 0x37, 0x3a, 0x51,
-	  0x69, 0x26, 0x38, 0x52, 0x9e, 0x9a, 0x4f, 0xa7,
-	  0x43, 0x10, 0x80, 0xee, 0x3d, 0x59, 0x35, 0xcf,
-	  0x79, 0x74, 0xb5, 0xa2, 0xb1, 0x96, 0x23, 0xe0,
-	  0xbe, 0x05, 0xf5, 0x6e, 0x19, 0xc5, 0x66, 0x49,
-	  0xf0, 0xd1, 0x54, 0xa9, 0x70, 0x4b, 0xa4, 0xe2,
-	  0xe6, 0xe5, 0xab, 0xe4, 0xd2, 0xaa, 0x4c, 0xe3,
-	  0x06, 0x6f, 0xc6, 0x4a, 0xa4, 0x75, 0x97, 0xe1
-	};
-
-	int circlePos = 0;
-	byte x1, y1, penSize, bitPos = splatterStart[patNum];
-
-	penSize = (patCode & 7);
-
-	/* Don't know exactly what happens at the borders, but it can definitely
-	** cause problems if it isn't right. */
-	/*
-	if (x<((penSize/2)+1)) x=((penSize/2)+1);
-	else if (x>160-((penSize/2)+1)) x=160-((penSize/2)+1);
-	if (y<penSize) y = penSize;
-	else if (y>=168-penSize) y=167-penSize;
-	*/
-	if (x < penSize) x = penSize - 1;
-	if (y < penSize) y = penSize;
-	//else if (y>=168-penSize) y=167-penSize;
-
-	//TODO: Fix Float
-	//for (y1 = y - penSize; y1 <= y + penSize; y1++) {
-	//    for (x1 = x - (ceil((float)penSize / 2)); x1 <= x + (floor((float)penSize / 2)); x1++) {
-	//        if (patCode & 0x10) { /* Square */
-	//            plotPatternPoint();
-	//        }
-	//        else { /* Circle */
-	//            if ((circles[patCode & 7][circlePos >> 3] >> (7 - (circlePos & 7))) & 1) {
-	//                plotPatternPoint();
-	//            }
-	//            circlePos++;
-	//        }
-	//    }
-	//}
-
-}
-
-
 /**************************************************************************
 ** plotBrush
 **
@@ -930,7 +934,7 @@ byte b11PlotBrush(byte** data, BufferStatus* bufferStatus)
 
 		GET_NEXT(y1);
 		if (y1 >= 0xF0) return y1;
-		b11PlotPattern(x1, y1);
+		b4PlotPattern(x1, y1);
 	}
 }
 
