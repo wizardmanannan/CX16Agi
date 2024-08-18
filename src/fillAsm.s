52c98dac95ec90bb3c7f3dfc9c2b890045a0b33f
andrew <andrewjohnlay@gmail.com>
2024-08-18 16:15:51 +1000

    Now, in can fill for priority comparing priority to 40 rather than shifting and comparing to 4

--- src/fillAsm.s
+++ src/fillAsm.s
@@ -130,19 +130,18 @@ rts
     @odd:
     lda VERA_data0
     and #$0F
-    bra @comparePriority
+    cmp #4
+    bne cannot_fill
+    bra vis_enabled_check
 
     @even:
     lda VERA_data0
     and #$F0
-    lsr
-    lsr
-    lsr
-    lsr
+    cmp #$40
+    bne cannot_fill
     
     @comparePriority:
-    cmp #4
-    bne cannot_fill
+
 
 vis_enabled_check:
     ; is priority enabled and the current vis pixel not white?
@@ -330,19 +329,17 @@ sta VERA_addr_bank
     @odd:
     tya
     and #$0F
-    bra @comparePriority
+    cmp #4
+    bne cannot_fill
+    bra vis_enabled_check
 
     @even:
     tya
     and #$F0
-    lsr
-    lsr
-    lsr
-    lsr
-    
-    @comparePriority:
-    cmp #4
+    cmp #$40
     bne cannot_fill
+    
+
 
 vis_enabled_check:
     ; is priority enabled and the current vis pixel not white?
