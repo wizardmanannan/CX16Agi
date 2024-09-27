0183b520b73fe8d5ce9614dc3bb87f38a53a9ad4
andrew <andrewjohnlay@gmail.com>
2024-09-27 21:49:18 +1000

    Draft celToVera on low ram

--- src/viewAsm.s
+++ src/viewAsm.s
@@ -87,7 +87,7 @@ VERA_BYTES_PER_ROW = ZP_TMP_2
 BCOL = ZP_TMP_2 + 1
 VERA_ADDRESS = ZP_TMP_3
 VERA_ADDRESS_HIGH = ZP_TMP_4
-LOCAL_CEL = ZP_TMP_5
+CEL_ADDR = ZP_TMP_5
 BUFFER_STATUS = ZP_TMP_6 ;Takes Up 7 as well
 BUFFER_POINTER = ZP_TMP_8
 CEL_HEIGHT = ZP_TMP_9
@@ -101,6 +101,11 @@ PRIORITY_VERA_ADDRESS = ZP_TMP_14
 X_VAL = ZP_TMP_18 + 1 ;Ideally we would start from ZP_TMP_16 by TOTAL_ROWS cannot be moved for some reason TODO: Investigate
 Y_VAL = ZP_TMP_19
 P_NUM = ZP_TMP_19 + 1
+X_VAL_ORIG = ZP_TMP_20 
+CEL_BANK = ZP_TMP_20 + 1
+NEXT_DATA_INDEX = ZP_TMP_21
+
+
 
 
 ;Constants
@@ -115,10 +120,15 @@ NO_MARGIN = 4
 ;This function is priority screen aware, each pixel will only show through if the pNum >= the priority screen pixel at x,y.
 ;Note, as each priority byte on the priority screen stores two pixels, the auto increment bit is
 ;alternated after every read
-;The function stops when it has counted height number of zeros
-.macro CEL_TO_VERA
-GET_STRUCT_8_STORED_OFFSET _offsetOfCelHeight, LOCAL_CEL, CEL_HEIGHT
-GET_STRUCT_8_STORED_OFFSET _offsetOfCelTrans, LOCAL_CEL, CEL_TRANS
+;If the object is partially off screen in the X direction the individual line drawing will stop at the screen boundary.
+;The function stops when it has counted height number of zeros, or the line would not be in the drawable area, vertically.
+.macro CEL_TO_VERA_BANKED_BUFFER
+lda X_VAL 
+sta X_VAL_ORIG ;Keep track of the original X value
+
+
+GET_STRUCT_8_STORED_OFFSET _offsetOfCelHeight, CEL_ADDR, CEL_HEIGHT
+GET_STRUCT_8_STORED_OFFSET _offsetOfCelTrans, CEL_ADDR, CEL_TRANS
 stz BUFFER_STATUS + 3
 
 lda SPLIT_SEGMENTS
@@ -126,8 +136,8 @@ cmp #$1
 bne @split
 
 @nonSplit:
-GET_STRUCT_16_STORED_OFFSET _offsetOfBmp, LOCAL_CEL, BUFFER_STATUS ;Buffer status holds the C struct to be passed to b5RefreshBuffer
-GET_STRUCT_8_STORED_OFFSET _offsetOfBmpBank, LOCAL_CEL, BUFFER_STATUS + 2
+GET_STRUCT_16_STORED_OFFSET _offsetOfBmp, CEL_ADDR, BUFFER_STATUS ;Buffer status holds the C struct to be passed to b5RefreshBuffer
+GET_STRUCT_8_STORED_OFFSET _offsetOfBmpBank, CEL_ADDR, BUFFER_STATUS + 2
 bra @start
 
 @split:
@@ -154,6 +164,10 @@ stz _logDebugVal1
 lda CEL_TRANS
 SET_COLOR_LEFT CEL_TRANS
 sta CEL_TRANS
+
+@resetXCounter:
+lda X_VAL_ORIG ;Restore the original X value
+sta X_VAL
 
 @setVeraAddress:
 SET_VERA_ADDRESS VERA_ADDRESS, #$1, VERA_ADDRESS_HIGH, #$0
@@ -197,11 +211,16 @@ eor VERA_addr_bank
 sta VERA_addr_bank
 pla
 
+inc X_VAL ;Increment the x val. Note this is not used in calculations only for bounds checks
 dey ;Have we handled every pixel in this run encoded byte
 beq @getNextChunk
 bra @skip
 
 @draw:
+ldx X_VAL
+cpx #PICTURE_WIDTH ;If X is larger than the width, the object is at least partially off screen we skip drawing the rest of the pixels on this line
+bcs @skip 
+
 @drawBlack:
 cmp #BLACK_COLOR
 bne @checkPriority
@@ -267,8 +286,28 @@ sta VERA_ADDRESS_HIGH
 
 inc Y_VAL
 
-jmp @setVeraAddress
+lda Y_VAL ;If Y_VAL is greater than the height then the object is partially off screen, stop drawing altogether
+cmp #PICTURE_HEIGHT
+bcs @end
 
+jmp @resetXCounter
+
+@end:
+.endmacro
+
+.macro READ_NEXT_BYTE
+.local @increment
+.local @end
+
+ldy NEXT_DATA_INDEX
+lda (CEL_DATA), y
+
+inc NEXT_DATA_INDEX
+
+bne @end
+
+@increment:
+inc CEL_DATA + 1 ;Adding 256 0x100 which is adding zero to the low byte and 1 to the high
 @end:
 .endmacro
 
@@ -288,13 +327,13 @@ jsr popax
 sta VERA_ADDRESS_HIGH
 stx VERA_ADDRESS_HIGH + 1
 jsr popax
-sta LOCAL_CEL
-stx LOCAL_CEL + 1
+sta CEL_ADDR
+stx CEL_ADDR + 1
 lda #$1
 sta SPLIT_SEGMENTS ;When we draw directly to the bitmap we don't need to split the cel into segments
 
 sei
-CEL_TO_VERA
+CEL_TO_VERA_BANKED_BUFFER
 REENABLE_INTERRUPTS
 
 rts
@@ -330,9 +369,9 @@ lda _sizeofCel
 sta SIZE_OF_CEL
 
 lda #<_bEToBlitCelArray
-sta LOCAL_CEL
+sta CEL_ADDR
 lda #>_bEToBlitCelArray
-sta LOCAL_CEL + 1
+sta CEL_ADDR + 1
 
 ;Height:
 jsr popa
@@ -409,8 +448,8 @@ bra @loop
 lda #$0
 sta SPLIT_COUNTER
 
-GET_STRUCT_16_STORED_OFFSET _offsetOfSplitCelPointers, LOCAL_CEL, SPLIT_CEL_SEGMENTS
-GET_STRUCT_8_STORED_OFFSET _offsetOfSplitCelBank, LOCAL_CEL, SPLIT_CEL_BANK
+GET_STRUCT_16_STORED_OFFSET _offsetOfSplitCelPointers, CEL_ADDR, SPLIT_CEL_SEGMENTS
+GET_STRUCT_8_STORED_OFFSET _offsetOfSplitCelBank, CEL_ADDR, SPLIT_CEL_BANK
 
 lda SPLIT_CEL_SEGMENTS
 ora SPLIT_CEL_SEGMENTS + 1
@@ -437,7 +476,7 @@ ldx #$0
 jsr _memCpyBankedBetween
 
 @splitLoop: ;Will always iterate once if the cell is not split
-GET_STRUCT_8_STORED_OFFSET _offsetOfSplitSegments, LOCAL_CEL, SPLIT_SEGMENTS
+GET_STRUCT_8_STORED_OFFSET _offsetOfSplitSegments, CEL_ADDR, SPLIT_SEGMENTS
 
 ldy BULK_ADDRESS_INDEX
 
@@ -451,13 +490,13 @@ iny
 iny
 sty BULK_ADDRESS_INDEX
 
-GET_STRUCT_8_STORED_OFFSET _offsetOfCelTrans, LOCAL_CEL, CEL_TRANS
+GET_STRUCT_8_STORED_OFFSET _offsetOfCelTrans, CEL_ADDR, CEL_TRANS
 
 CLEAR_VERA VERA_ADDRESS, TOTAL_ROWS, BYTES_PER_ROW, #$0
 
 lda Y_VAL
 pha
-CEL_TO_VERA
+CEL_TO_VERA_BANKED_BUFFER
 pla
 sta Y_VAL
 
@@ -472,12 +511,12 @@ jmp @splitLoop
 
 @getNextCel:
 clc
-lda LOCAL_CEL
+lda CEL_ADDR
 adc SIZE_OF_CEL
-sta LOCAL_CEL
-lda LOCAL_CEL + 1
+sta CEL_ADDR
+lda CEL_ADDR + 1
 adc #$0
-sta LOCAL_CEL + 1
+sta CEL_ADDR + 1
 
 
 inc CEL_COUNTER
@@ -1206,4 +1245,191 @@ bra @padUnusedSegmentLoop
 
 @end:
 rts
+
+.segment "CODE"
+celToVeraLowRam:
+.scope
+lda X_VAL 
+sta X_VAL_ORIG ;Keep track of the original X value
+
+stz NEXT_DATA_INDEX
+
+lda RAM_BANK
+pha
+
+lda CEL_BANK
+sta RAM_BANK
+
+GET_STRUCT_8_STORED_OFFSET _offsetOfCelHeight, CEL_ADDR, CEL_HEIGHT
+GET_STRUCT_8_STORED_OFFSET _offsetOfCelTrans, CEL_ADDR, CEL_TRANS
+stz BUFFER_STATUS + 3
+
+lda SPLIT_SEGMENTS
+cmp #$1
+bne @split
+
+@nonSplit:
+GET_STRUCT_16_STORED_OFFSET _offsetOfBmp, CEL_ADDR, CEL_DATA ;Buffer status holds the C struct to be passed to b5RefreshBuffer
+GET_STRUCT_8_STORED_OFFSET _offsetOfBmpBank, CEL_ADDR, CEL_DATA_BANK
+bra @start
+
+@split:
+lda SPLIT_COUNTER
+asl
+tay
+
+lda bESplitAddressesBuffer, y
+sta BUFFER_STATUS 
+iny
+lda bESplitAddressesBuffer, y
+sta BUFFER_STATUS + 1
+
+lda SPLIT_CEL_BANK
+sta BUFFER_STATUS + 2
+
+@start:
+
+.ifdef DEBUG_VIEW_DRAW
+stz _logDebugVal1
+.endif
+
+lda CEL_TRANS
+SET_COLOR_LEFT CEL_TRANS
+sta CEL_TRANS
+
+lda CEL_DATA_BANK
+sta RAM_BANK
+
+@resetXCounter:
+lda X_VAL_ORIG ;Restore the original X value
+sta X_VAL
+
+@setVeraAddress:
+SET_VERA_ADDRESS VERA_ADDRESS, #$1, VERA_ADDRESS_HIGH, #$0
+ldy Y_VAL
+CALC_VRAM_ADDR_PRIORITY X_VAL, #$1
+@calculatePriorityAutoInc: ;This calculates whether the priority auto incrementment should be initially switched on or not. If X even auto inc should be off, because we need to read the same byte for the next turn
+lda X_VAL
+lsr
+bcs @oddValue
+
+@evenValue:
+stz VERA_addr_bank ;Disable, we need to read this byte again
+bra @getNextChunk
+
+@oddValue:
+lda #%10000 ;Odd on the next read we should move onto the next byte
+sta VERA_addr_bank
+
+@getNextChunk:
+READ_NEXT_BYTE
+DEBUG_VIEW_DRAW
+cmp #$0 ;If its zero we are finished with this line
+beq @increment
+tax
+and #$0F; The number of pixels is the lower 4 bits
+tay
+txa
+and #$F0; The colour is the upper 4 bits
+sta OUTPUT_COLOUR
+SET_COLOR_RIGHT OUTPUT_COLOUR ;Output color must be 'doubled up', because AGI pixels are doubled across
+
+cmp CEL_TRANS
+bne @draw
+
+@skip: ;When 'drawing' transparent pixels we still need to increment the address
+ldx VERA_data0 ;We are not changing this one so we load load in order to increment and ignore the value
+ldx VERA_data1
+pha ;Toggle the priority auto increment and preserve the color
+lda #%10000
+eor VERA_addr_bank
+sta VERA_addr_bank
+pla
+
+inc X_VAL ;Increment the x val. Note this is not used in calculations only for bounds checks
+dey ;Have we handled every pixel in this run encoded byte
+beq @getNextChunk
+bra @skip
+
+@draw:
+ldx X_VAL
+cpx #PICTURE_WIDTH ;If X is larger than the width, the object is at least partially off screen we skip drawing the rest of the pixels on this line
+bcs @skip 
+
+@drawBlack:
+cmp #BLACK_COLOR
+bne @checkPriority
+lda CEL_TRANS ;Black is swapped with the transparent colour in the case the transparent colour is not black
+
+@checkPriority:
+ldx VERA_data1 ;Get the next priority byte and toggle
+pha ;Preserve the color
+lda #%10000
+eor VERA_addr_bank
+sta VERA_addr_bank
+
+bne @getEvenValue ;If we toggled this to be on then that means we will read the byte one more time, and then increment. This must mean that this time we are reading the even nibble
+
+@getOddValue:
+txa ;Read the right hand side
+and #$0F
+bra @comparePriority
+
+@getEvenValue:
+txa ;Read the left hand side
+lsr 
+lsr
+lsr
+lsr
+
+@comparePriority:
+cmp #NOT_AN_OBSTACLE
+beq @drawColor ;4 is the lowest priority and the object always has precedence 
+bcc @drawColor ;TODO: Is a control value need to use the rules in split priority to figure out what the priority is
+cmp P_NUM
+beq @drawColor ;If the object and screen priority is equal the object has precedence
+bcc @drawColor ;If the object screen priority < object priority the object has precedence
+
+@skipBasedOnPriority: ;This means that the screen priority > object priority, so we don't draw
+lda VERA_data0
+pla ;Retrieve the color
+bra @decrementColorCounter
+
+@drawColor: ;This means that the screen priority <= object priority, so we do draw
+pla ;Retrieve the color
+sta VERA_data0
+
+@decrementColorCounter:
+dey
+bne @draw ;If y is not zero we are not yet finished with this run encoded byte
+jmp @getNextChunk
+
+@increment:
+dec CEL_HEIGHT ;Ready for the next line
+beq @end
+
+clc
+lda BYTES_PER_ROW 
+adc VERA_ADDRESS
+sta VERA_ADDRESS
+lda #$0
+adc VERA_ADDRESS + 1
+sta VERA_ADDRESS + 1
+lda #$0
+adc VERA_ADDRESS_HIGH
+sta VERA_ADDRESS_HIGH
+
+inc Y_VAL
+
+lda Y_VAL ;If Y_VAL is greater than the height then the object is partially off screen, stop drawing altogether
+cmp #PICTURE_HEIGHT
+bcs @end
+
+jmp @resetXCounter
+
+@end:
+pla 
+sta RAM_BANK
+rts
+.endscope
 
