96d10c55430c8c1400057b3734b21cd15c8204cc
alay <andrewjohnlay@gmail.com>
2025-11-23 03:59:20 -0800

    Reapply "Removed drawobj function"
    This reverts commit 95f4a1fed449a12eae54e60620406d5a94b8ba68.

--- src/view.h
+++ src/view.h
@@ -63,7 +63,6 @@ extern void bAInitObjects();
 extern void bAWander(ViewTable* localViewTab, byte entryNum);
 extern void bAFindPosition(int entryNum, ViewTable* viewTab);
 extern void bBUpdateObj(int entryNum);
-extern void bADrawObject(ViewTable* viewTab, byte entryNum);
 extern void bAResetViews();
 #pragma wrapped-call (pop)
 
