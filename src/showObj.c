4782cf8b3a244f4c48e828d27317d763e45d079b
alay <andrewjohnlay@gmail.com>
2026-09-12 18:07:40 +1000

    Coming up with an out of memory flow. Marking some things as only for split sprites

--- src/showObj.c
+++ src/showObj.c
@@ -7,7 +7,6 @@ extern byte b11ShowObjY;
 extern byte b11VeraSlots;
 
 
-boolean trap = FALSE;
 #pragma code-name (push, "BANKRAM11")
 void b11ShowObj(byte objNum)
 {
@@ -43,12 +42,14 @@ void b11ShowObj(byte objNum)
 
    if (!outOfSpriteMemory)
    {
-      *((byte*)SPLIT_COUNTER) = 1;
+      if (localView.maxVeraSlots > 1)
+      {
+         *((byte*)SPLIT_COUNTER) = 1;
+      }
+
       for (i = 0; i < localView.maxVeraSlots; i++)
       {
          asm("sei");
-
-         trap = TRUE;
 
          SET_VERA_ADDRESS_ZP(spriteAddress[i], VERA_ADDRESS, VERA_ADDRESS_HIGH);
          bEClearVeraSprite(localLoop.allocationWidth, localLoop.allocationHeight);
@@ -57,13 +58,15 @@ void b11ShowObj(byte objNum)
          if (localView.maxVeraSlots > 1)
          {
             *((byte*)(SPLIT_CEL_BANK)) = localCel.splitCelBank;
-
             *((byte***)(SPLIT_CEL_SEGMENTS)) = localCel.splitCelPointers;
          }
 
          b9CelToVera(&localCel, localLoop.celsBank, spriteAddress[i], MAX_PRIORITY, spriteAllocationWidth / 2, 0, 0, MAX_PRIORITY, localView.maxVeraSlots);
 
-         (*((byte*)SPLIT_COUNTER))++;
+         if (localView.maxVeraSlots > 1)
+         {
+            (*((byte*)SPLIT_COUNTER))++;
+         }
       }
 
       b11ShowObjX = PICTURE_WIDTH - localCel.width;
@@ -73,33 +76,34 @@ void b11ShowObj(byte objNum)
 
       b11VeraSlots = localView.maxVeraSlots;
       b6SetAndWaitForIrqState(SHOW_OBJ);
-
-      b3DisplayMessageBox(localView.description, localView.codeBlockBank, AUTO_CALC_ROW, AUTO_CALC_COLUMN, TEXTBOX_PALETTE_NUMBER, DEFAULT_BOX_WIDTH, TRUE);
-
-      do
-      {
-         GET_IN(ch);                     // Get keyboard input
-
-      } while (ch != KEY_ESC && ch != KEY_ENTER);
-
-      b3ClearLastPlacedText();
-      b6SetAndWaitForIrqState(CLEAR_OBJ);
-
-      for (i = 0; i < localView.maxVeraSlots; i++)
-      {
-         if (spriteAddress[i])
-         {
-            bDDeleteAllocation(spriteAddress[i], spriteAllocationWidth, spriteAllocationHeight);
-         }
-      }
-
-      if(paletteGetResult == Allocated) //We only have so many palettes, if we allocate a palette which is not in used by one of the game sprites we should dispose it of
-      {
-         bFRemoveLastPalette();
-      }
-
-      b9DiscardView(objNum);
    }
+
+   b3DisplayMessageBox(localView.description, localView.codeBlockBank, AUTO_CALC_ROW, AUTO_CALC_COLUMN, TEXTBOX_PALETTE_NUMBER, DEFAULT_BOX_WIDTH, TRUE);
+
+   do
+   {
+      GET_IN(ch);                     // Get keyboard input
+
+   } while (ch != KEY_ESC && ch != KEY_ENTER);
+
+   b3ClearLastPlacedText();
+   b6SetAndWaitForIrqState(CLEAR_OBJ); //Doesn't matter is we clear the sprite attributes if we didn't display anything due to out of memory; this just deactivates sprites, which will have no effect if they were never displayed
+
+
+   for (i = 0; i < localView.maxVeraSlots; i++)  //We might have enough memory to allocate one part of the object but not the other; this is why this is not inside the if (!outOfSpriteMemory)
+   {
+      if (spriteAddress[i])
+      {
+         bDDeleteAllocation(spriteAddress[i], spriteAllocationWidth, spriteAllocationHeight);
+      }
+   }
+
+   if (paletteGetResult == Allocated) //We only have so many palettes, if we allocate a palette which is not in used by one of the game sprites we should dispose it of
+   {
+      bFRemoveLastPalette();
+   }
+
+   b9DiscardView(objNum);
 }
 
 #pragma code-name (pop)
